481418119c506d277d77978bb2906bfc0dae9835
Himanshu Thakur <hthakur@enestit.com>
2021-02-01 21:10:55 +0530

    misc

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Array;
+using System;
 
 namespace DataStructures
 {
@@ -6,7 +7,26 @@ namespace DataStructures
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] arr = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+
+			//LinearSearchElementAndPrintIndex(arr, 60);
+
+			int[] arr1 = { 2, 3, 4, 10, 40 };
+			Console.WriteLine(Searching.BinarySearch(arr1, 50));
 		}
+
+		#region PRIVATE METHODS
+
+		private static void LinearSearchElementAndPrintIndex(int[] arr, int elementToSearch)
+		{
+			int position = Searching.LinearSearch(arr, elementToSearch);
+
+			string result = (position == -1) ? $"Element {elementToSearch} is not present in the given array"
+				: $"Element {elementToSearch} is present at index {position}";
+
+			Console.WriteLine(result);
+		}
+
+		#endregion
 	}
 }
